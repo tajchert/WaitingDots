2cc6b8634cc0abcd75493a0e7a3635021a351995
Marcin Korniluk <marcin.korniluk@polidea.com>
2015-03-25 13:56:45 +0100

    initial commit

---
+++ app/src/main/java/com/example/marcin/dots/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.marcin.dots;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View view = findViewById(R.id.progress);
+        view.setBackgroundDrawable(new DotsDrawable(0xffff0000));
+    }
+
+}
