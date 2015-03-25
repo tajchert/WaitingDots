fc73ab418ac016423a48a9d82176b17073db4f4f
Michal Tajchert <michal.tajchert@polidea.com>
2015-03-25 17:11:37 +0100

    Drawable support moved to separate branch.

--- lib/src/main/java/pl/tajchert/waitingdots/DotsTextView.java
+++ lib/src/main/java/pl/tajchert/waitingdots/DotsTextView.java
@@ -11,7 +11,6 @@ public class DotsTextView extends LinearLayout {
     private TextView dotOne;
     private TextView dotTwo;
     private TextView dotThree;
-    private int directionSign = 1;
     private int textWidth;
     private int textHeight;
 
@@ -49,20 +48,11 @@ public class DotsTextView extends LinearLayout {
     @Override
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        //float time = (System.currentTimeMillis()-startTime)%JUMP_PERIOD;
         float time = (System.currentTimeMillis()-startTime)/JUMP_PERIOD;
 
-        /*if(time == JUMP_PERIOD - 1) {
-            if(directionSign == 1){
-                directionSign = (-1);
-            } else {
-                directionSign = 1;
-            }
-        }*/
 
         for (int i = 3; i >= 0; i--) {
             float y = (float) (this.textHeight / 2 - (this.textHeight - 15)*Math.max(0, Math.sin(time+i/3.0f)));
-            //int y = Math.round(time * directionSign);
             switch (i){
                 case 0:
                     dotOne.setPadding(0, -Math.round(y), 0, 0);
