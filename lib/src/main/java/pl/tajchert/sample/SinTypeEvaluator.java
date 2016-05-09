7010db55f6cdd49177008d9d10eae1c2b5317c9a
Julio Yaguez <julio@gmail.com>
2016-05-09 13:07:48 +0200

    - extracted TypeEvaluator anonymous class to an actual class and same for the update listeners

---
+++ lib/src/main/java/pl/tajchert/sample/SinTypeEvaluator.java
@@ -0,0 +1,10 @@
+package pl.tajchert.sample;
+
+import android.animation.TypeEvaluator;
+
+public class SinTypeEvaluator implements TypeEvaluator<Number> {
+    @Override
+    public Number evaluate(float fraction, Number from, Number to) {
+        return Math.max(0, Math.sin(fraction * Math.PI * 2)) * (to.floatValue() - from.floatValue());
+    }
+}
