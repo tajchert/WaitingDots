d4cd48d1dcc4a5263a775a1e0dc58c4b337865b8
Michal Tajchert <mtajchert@gmail.com>
2019-06-08 16:57:04 +0200

    Update app compat and build tools to 28. MinSDK is 14.

--- sample/src/main/java/pl/tajchert/waitingdots/sample/MainActivity.java
+++ sample/src/main/java/pl/tajchert/waitingdots/sample/MainActivity.java
@@ -1,14 +1,14 @@
 package pl.tajchert.waitingdots.sample;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import pl.tajchert.waitingdots.DotsTextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
     DotsTextView dotsTextView;
     Button buttonPlay;
     Button buttonHide;
